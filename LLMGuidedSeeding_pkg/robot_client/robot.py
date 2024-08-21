3dc3859dbc98d1b790adf3f35b0715430b9b39af
suchkristen1@gmail.com <suchkristen1@gmail.com>
2024-08-21 15:27:37 -0600

    about to pull to the husky

--- LLMGuidedSeeding_pkg/robot_client/robot.py
+++ LLMGuidedSeeding_pkg/robot_client/robot.py
@@ -13,6 +13,7 @@ import time
 import octomap
 from LLMGuidedSeeding_pkg import *
 from collections import deque 
+import toml 
 
 class identified_object: 
     def __init__(self,object_id,init_observation,label,history_size=10): 
@@ -42,6 +43,8 @@ class identified_object:
 
         self.mu = mu_pred + K @ y  # Updated state estimate
         self.sigma = (np.eye(2) - K @ H) @ sigma_pred 
+
+        self.plot_colors = {} 
 
 def pixel_to_ray(pixel_coord, camera_intrinsics):
     """Convert a pixel coordinate to a normalized ray in the camera frame."""
@@ -87,14 +90,26 @@ def ray_cast_octomap(octomap_obj, origin, direction):
         return None  # No intersection found
     
 class Robot:
-    def __init__(self, config_path, vehicle_prefix='/H03'):
+    def __init__(self, config_path, plot_bounds, vehicle_prefix='/H03'):
         self.static_transformer = robotTransforms(config_path)
         
         with open(config_path, "r") as f:
             self.settings = toml.load(f)
 
+        self.plot_bounds = plot_bounds 
+        if not np.array_equal(self.plot_bounds[0], self.plot_bounds[-1]):
+            self.plot_bounds = np.vstack([self.plot_bounds, self.plot_bounds[0]])   
+        
+        self.maxD = self.settings["robot"]["vision_range"] 
+        self.fov = self.settings["robot"]["front_cam_fov_deg"] 
         self.observation_frequency  = self.settings["robot"]["observation_frequency"]
         self.confidence_threshold = self.settings["robot"]["confidence_threshold"]
+
+        self.robot_length = self.settings["robot"]["husky_length"]
+        self.robot_width = self.settings["robot"]["husky_width"]  
+
+        self.traj_cache = np.zeros((1,6)); 
+        self.observation_cache = {} #keys are tsteps, entries are results  
 
         self.ready_to_act = True 
         self.current_waypoint = None
@@ -104,6 +119,7 @@ class Robot:
         self.current_odom_msg = None
         self.planter_position = 0 
         self.planted_locations = []
+        self.observed_areas = []
 
         self.bridge = CvBridge()
 
@@ -160,7 +176,7 @@ class Robot:
         rate = rospy.Rate(self.observation_frequency) 
         while not rospy.is_shutdown():
             if self.has_moved():
-                self.get_current_observations()
+                self.get_current_observations() #everytime we move, we update the map 
             rate.sleep()
 
     def has_moved(self):
@@ -178,6 +194,20 @@ class Robot:
         # Consider the robot moved if it moved more than 0.01 meters or changed heading more than 0.01 radians
         return distance_moved > 0.01 or heading_changed > 0.01
     
+    def in_plot_bounds(self): 
+        bounds = Polygon(self.plot_bounds)
+        x = Point(self.get_current_pose()[:2])
+        return bounds.contains(x) 
+
+    def update_observed_areas(self,robot_pose): 
+        for camera in self.camera_names:
+            fov_coords = self.static_transformer.get_front_cam_fov(camera,robot_pose); 
+            if not np.array_equal(fov_coords[0], fov_coords[-1]):
+                fov_coords = np.vstack([fov_coords, fov_coords[0]]) 
+            fov = Polygon(fov_coords) 
+            self.observed_areas.append(fov)  
+
+
     #CONTROL 
     def follow_path(self):  
         if not self.ready_to_act:
@@ -278,7 +308,7 @@ class Robot:
     def odometry_callback(self, msg):
         """Handle odometry messages."""
         self.current_odom_msg = msg
-    
+                
     #IMAGE DETECTIONS 
     def check_environment_for_something(self,thing): 
         if thing in self.current_map.keys():
@@ -382,6 +412,15 @@ class Robot:
 
         return depth
 
+    def get_new_object_id(self): 
+        max_id = 0 
+        for label in self.current_map.keys():
+            label_objs = self.current_map[label]
+            label_max_id = max([x.obj_id for x in label_objs]) 
+            if label_max_id > max_id:
+                max_id = label_max_id
+        return max_id + 1
+    
     def update_map(self,results):   
         """
         Update the robot's map with new observations.
@@ -417,29 +456,183 @@ class Robot:
 
                 object_center = CamProjector(depth,camera_topic,camera_pose,robot_pose) 
 
-                # If this label is already in the map, update the nearest object
-                if label in self.current_map:
-                    if not label in self.sized_labels: 
-                        label_size = self.get_label_size(label) 
-                    else:
-                        label_size = self.sized_labels[label] 
-
+               if label in self.current_map:
+                    label_size = 0.3
                     existing_objects = self.current_map[label]
-
                     # Find the closest existing object using Euclidean distance
                     distances = [np.linalg.norm(obj.mu - object_center) for obj in existing_objects]
-                    min_dist = min(distances)
+                    min_dist = min(distances); idx = np.argmin(distances)
+                    euc_dist = np.linalg.norm(existing_objects[idx].mu - object_center)
+                    nearest_object = existing_objects[distances.index(min_dist)]
+                    #print("nearest object id: ",existing_objects[idx].object_id) 
 
-                    if min_dist < label_size * 1.25:  # Threshold for association (1.0 meter in this example)
-                        nearest_object = existing_objects[distances.index(min_dist)]
+                    if min_dist < label_size * 2:     
                         nearest_object.integrate_new_observation(object_center)
                     else:
-                        # If no nearby object found, create a new one
-                        #object_id,init_observation,label
-                        id_ = max([x.object_id for x in existing_objects]) + 1
-                        new_object = identified_object(id_, object_center, label)
-                        self.current_map[label].append(new_object)
+                        d_m = mahalanobis_distance(object_center, existing_objects[idx].mu, existing_objects[idx].sigma * 10) 
+                        likelihood = gaussian_likelihood(object_center,existing_objects[idx].mu, existing_objects[idx].sigma * 10) 
+                        #print("likelihood: ",likelihood)
+
+                        if 0.05 < likelihood :
+                            #print("integrating measurement...")
+                            nearest_object.integrate_new_observation(object_center)
+                        else: 
+                            r = np.random.rand()
+                            if d_m < label_size*25: 
+                                if  r < likelihood * (label_size/euc_dist): 
+                                    #print("integrating measurement...")
+                                    nearest_object.integrate_new_observation(object_center) 
+                                else: 
+                                    id_ = self.get_new_object_id() 
+                                    new_object = identified_object(id_, object_center, label)
+                                    self.current_map[label].append(new_object) 
+                            else: 
+                                id_ = self.get_new_object_id() 
+                                new_object = identified_object(id_, object_center, label)
+                                self.current_map[label].append(new_object) 
                 else:
                     # If this label is not in the map, create a new entry
-                    #object_id,init_observation,label
-                    self.current_map[label] = [identified_object(0, object_center, label)]
+                    id_ = self.get_new_object_id() 
+                    self.current_map[label] = [identified_object(id_, object_center, label)]  
+
+    def check_all_observed(self):
+        '''
+        Want to keep track of which areas weve already observed within the plot bounds. Returns [] if we have observed the entire area of the plot bounds 
+        else the closest point from the current pose which would allow us to observe a currently unobserved area
+        '''
+        remaining_area = Polygon(self.plot_bounds)  # Start with the entire plot_bounds
+
+        if len(self.observed_areas) == 0:
+            self.get_current_observations()
+
+        for observed_area in self.observed_areas:
+            remaining_area = remaining_area.difference(observed_area)
+
+        # Check if the result is a MultiPolygon or a single Polygon
+        if remaining_area.is_empty:
+            return True  # No unobserved areas
+        else:
+            return False 
+    
+    def check_environment_for_something(self,thing): 
+
+        if self.current_tstep not in self.observation_cache.keys(): 
+            self.get_current_observations()
+
+        if thing in self.current_map.keys():
+            things = self.current_map[thing]
+
+            thing_locations = np.array([x.mu for x in things]); thing_locations = np.reshape(thing_locations,(len(things),2))
+            #return the one thats closest
+            distances = np.sum((thing_locations - self.get_current_pose()[:2]) ** 2, axis=1)
+            
+            # Find the index of the smallest distance
+            closest_index = np.argmin(distances)
+            
+            # Return the location with the smallest distance
+            return thing_locations[closest_index],things[closest_index].object_id
+        
+        else:
+            return [],-1
+    
+    #PLOTTING FUNCTIONS 
+                    
+    def plot_robot(self):
+        self.ax.scatter(self.traj_cache[-1,0],self.traj_cache[-1,1],color="k")  
+    
+        x = self.traj_cache[-1,0]; y = self.traj_cache[-1,1]; yaw = self.traj_cache[-1,-1] 
+            
+        # Calculate the bottom-left corner of the rectangle considering the yaw angle
+        corner_x = x - self.robot_length * np.cos(yaw) + (self.robot_width / 2) * np.sin(yaw)
+        corner_y = y - self.robot_length * np.sin(yaw) - (self.robot_width / 2) * np.cos(yaw) 
+
+        # Create the rectangle patch
+        robot_rect = patches.Rectangle(
+            (corner_x, corner_y), self.robot_length, self.robot_width,
+            angle=np.degrees(yaw), edgecolor='black', facecolor='yellow', alpha=0.5
+        )
+
+        # Add the rectangle to the plot
+        self.ax.add_patch(robot_rect)
+
+        # Add an arrow to indicate the heading
+        arrow_length = 0.5 * self.robot_length
+        arrow_dx = arrow_length * np.cos(yaw)
+        arrow_dy = arrow_length * np.sin(yaw)
+        self.ax.arrow(x, y, arrow_dx, arrow_dy, head_width=0.1, head_length=0.1, fc='k', ec='k')         
+    
+    def plot_frame(self): 
+        print("plot_frame was called!") 
+
+        self.ax.clear() 
+        # Plot Robot & Pointer, and Bounds 
+        self.ax.plot(self.plot_bounds[:,0],self.plot_bounds[:,1],color="k") 
+
+        self.ax.set_aspect('equal') 
+        
+        self.plot_robot()
+        
+        # Plot traversed trajectory 
+        self.ax.plot(self.traj_cache[:,0],self.traj_cache[:,1],linestyle="--")  
+
+        #Plot the observations 
+        if self.current_tstep in self.observation_cache: 
+            observations_t = self.observation_cache[self.current_tstep]  
+            for i in range(len(observations_t["front"]["coords"])): 
+                observed_coord = observations_t["front"]["coords"][i]
+                self.ax.plot([self.traj_cache[-1,0], observed_coord[0]],[self.traj_cache[-1,1], observed_coord[1]],color="red",linestyle="--")
+            for i in range(len(observations_t["left"]["coords"])): 
+                observed_coord = observations_t["left"]["coords"][i]
+                self.ax.plot([self.traj_cache[-1,0], observed_coord[0]],[self.traj_cache[-1,1], observed_coord[1]],color="red",linestyle="--")
+            for i in range(len(observations_t["right"]["coords"])): 
+                observed_coord = observations_t["right"]["coords"][i]
+                self.ax.plot([self.traj_cache[-1,0], observed_coord[0]],[self.traj_cache[-1,1], observed_coord[1]],color="red",linestyle="--")
+        
+        #Plot the current map 
+        for obj_label in self.current_map: 
+            if obj_label not in self.plot_colors.keys(): 
+                self.plot_colors[obj_label] = list(np.random.choice(range(256), size=3))
+
+            objs = self.current_map[obj_label] 
+            for i,obj in enumerate(objs): 
+                obj_loc = obj.mu 
+                if i == 0:
+                    self.ax.scatter(obj_loc[0],obj_loc[1],color=self.plot_colors[obj_label],label=obj_label)  
+                else:
+                    self.ax.scatter(obj_loc[0],obj_loc[1],color=self.plot_colors[obj_label])   
+                self.ax.text(obj_loc[0]+ 0.1,obj_loc[1] + 0.1,str(obj.object_id)) 
+
+        #Plot seeded locations 
+        for i,location in enumerate(self.planted_locations):
+            if i == 0:
+                self.ax.scatter(location[0],location[1],color="orange",marker="*",s=15,label="planted location")
+            else: 
+                self.ax.scatter(location[0],location[1],color="orange",marker="*",s=15) 
+
+        if self.current_waypoint is not None:
+            try: 
+                self.ax.scatter(self.current_waypoint[0],self.current_waypoint[1],color="m",label="goal waypoint") 
+            except: 
+                pt = self.current_waypoint[0]
+                self.ax.scatter(pt.x,pt.y,color="m",label="goal waypoint")  
+
+        # Add a scale bar
+        scalebar = AnchoredSizeBar(self.ax.transData,
+                                size=1,         # The length of the scale bar in data units
+                                label='1 m',  # The label for the scale bar
+                                loc='lower right',  # Location of the scale bar
+                                pad=0.1,         # Padding between the bar and label
+                                borderpad=0.5,   # Padding between the scale bar and the plot
+                                sep=5,           # Separation between the bar and label
+                                frameon=False)   # Turn off the surrounding box/frame
+
+        # Add the scale bar to the plot
+        self.ax.add_artist(scalebar)
+
+        plt.legend() 
+        plt.pause(0.1) 
+        
+        #if not os.path.exists("test_frames"):
+        #    os.mkdir("test_frames") 
+
+        #self.fig.savefig("test_frames/frame"+str(self.current_tstep).zfill(5)+".png") 
